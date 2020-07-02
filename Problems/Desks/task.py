90e2d6c214b618ade0f99460266d8445f59aeb46
alexey-yubi <50112636+alexey-yubi@users.noreply.github.com>
2020-07-02 15:32:35 +0300

    initial

---
+++ Problems/Desks/task.py
@@ -0,0 +1,5 @@
+# put your python code here
+first_class = abs(int(input()))
+second_class = abs(int(input()))
+third_class = abs(int(input()))
+print(first_class // 2 + first_class % 2 + second_class // 2 + second_class % 2 + third_class // 2 + third_class % 2)
---
+++ Problems/The sum of digits/task.py
@@ -0,0 +1,3 @@
+# put your python code here
+digit = input()
+print(int(digit[0]) + int(digit[1]) + int(digit[2]))
